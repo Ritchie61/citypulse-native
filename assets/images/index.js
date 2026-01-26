c5e08f11a6423c814172cf3e737b973f49e16cef
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2026-01-26 10:21:03 +1000

    Create index.js

---
+++ assets/images/index.js
@@ -0,0 +1,12 @@
+// assets/images/index.js
+
+export const Images = {
+  // Home icons
+  iconHome: require('./icon_home.png'),
+  iconHomeHD: require('./icon_home@2x.png'),
+  iconHomeXHD: require('./icon_home@3x.png'),
+
+  // Profile icons
+  iconProfile: require('./icon_profile.png'),
+  // Add more images here
+};
