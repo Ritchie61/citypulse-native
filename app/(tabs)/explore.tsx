390ee97a7facd596f007410b892e35dfc1d95f80
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2026-01-26 17:12:41 +1000

    Update image source and add collapsible sections

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -31,6 +31,8 @@ export default function TabTwoScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
+
+      {/* Collapsible Sections */}
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
@@ -45,26 +47,32 @@ export default function TabTwoScreen() {
           <ThemedText type="link">Learn more</ThemedText>
         </ExternalLink>
       </Collapsible>
+
       <Collapsible title="Android, iOS, and web support">
         <ThemedText>
           You can open this project on Android, iOS, and the web. To open the web version, press{' '}
           <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
         </ThemedText>
       </Collapsible>
+
       <Collapsible title="Images">
         <ThemedText>
           For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
           <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
           different screen densities
         </ThemedText>
+
+        {/* Replaced template image with your actual logo */}
         <Image
-          source={require('@/assets/images/react-logo.png')}
+          source={require('../../assets/images/icon-home.png')}
           style={{ width: 100, height: 100, alignSelf: 'center' }}
         />
+
         <ExternalLink href="https://reactnative.dev/docs/images">
           <ThemedText type="link">Learn more</ThemedText>
         </ExternalLink>
       </Collapsible>
+
       <Collapsible title="Light and dark mode components">
         <ThemedText>
           This template has light and dark mode support. The{' '}
@@ -75,6 +83,7 @@ export default function TabTwoScreen() {
           <ThemedText type="link">Learn more</ThemedText>
         </ExternalLink>
       </Collapsible>
+
       <Collapsible title="Animations">
         <ThemedText>
           This template includes an example of an animated component. The{' '}
